544c1029b913067fb1113d9517e308284792f4a2
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2024-04-29 01:25:52 -0600

    Support for writing bundles
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- internal/cmd/attest.go
+++ internal/cmd/attest.go
@@ -1,12 +1,14 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 
 	intoto "github.com/in-toto/in-toto-golang/in_toto"
+	"github.com/puerco/bind/pkg/bundle"
 	"github.com/spf13/cobra"
 	"github.com/stacklok/trusty-attest/internal/packages"
 	"github.com/stacklok/trusty-attest/pkg/sbom"
@@ -30,14 +32,13 @@ func (o *attestOptions) AddFlags(cmd *cobra.Command) {
 		false,
 		"create a signed sigstore bundle (runs the oauth flow)",
 	)
-
 }
 
 func addAttest(parentCmd *cobra.Command) {
 	opts := attestOptions{}
 	createCmd := &cobra.Command{
 		Short:             fmt.Sprintf("%s attest: generates a trusty attestation", appname),
-		Use:               "attest [flags] [product_id [vuln_id [status]]]",
+		Use:               "attest repository/path/",
 		Example:           fmt.Sprintf("%s attest repository/path/ ", appname),
 		SilenceUsage:      false,
 		SilenceErrors:     true,
@@ -65,15 +66,43 @@ func addAttest(parentCmd *cobra.Command) {
 				return fmt.Errorf("building attestation predicate: %w", err)
 			}
 
+			// Create the attestation
 			att, err := trusty.Attest([]intoto.Subject{}, pred)
 
 			f := os.Stdout
-			enc := json.NewEncoder(f)
+			b := bytes.Buffer{}
+			enc := json.NewEncoder(&b)
 			enc.SetIndent("", "  ")
 			enc.SetEscapeHTML(false)
 
 			if err := enc.Encode(att); err != nil {
 				return fmt.Errorf("encoding attestation: %w", err)
+			}
+
+			if !opts.Bundle {
+				if _, err := b.WriteTo(f); err != nil {
+					return err
+				}
+				return nil
+			}
+
+			// If bundle, bind the attestation, this kicks off the
+			// sigstore flow
+			signer := bundle.NewSigner()
+			bndl, err := signer.SignAndBind(ctx, b.Bytes())
+			if err != nil {
+				return fmt.Errorf("signing and binding attestation: %w", err)
+			}
+
+			// Clear the buffer
+			b.Truncate(0)
+
+			if err := enc.Encode(bndl); err != nil {
+				return fmt.Errorf("encoding bundle: %w", err)
+			}
+
+			if _, err := b.WriteTo(f); err != nil {
+				return err
 			}
 
 			return nil
--- main.go
+++ main.go
@@ -6,24 +6,4 @@ import (
 
 func main() {
 	cmd.Execute()
-	/*
-		l := packages.NewLister()
-		ctx := context.Background()
-		if len(os.Args) == 1 {
-			logrus.Fatal("no directory specified")
-		}
-
-		nodelist, err := l.ReadPackages(ctx, os.Args[1])
-		if err != nil {
-			logrus.Fatal(err)
-		}
-
-		scorer := sbom.NewScorer()
-		results, err := scorer.ScoreNodeList(ctx, nodelist)
-		if err != nil {
-			logrus.Fatal(err)
-		}
-
-		fmt.Printf("%+v", results)
-	*/
 }
